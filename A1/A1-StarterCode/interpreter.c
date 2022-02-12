6f5c2fbca0bf0b27b54e08f57fd0e2de84daffb7
RuoyuDeng <rockdenggg@gmail.com>
2022-02-12 10:11:07 -0500

    merged version

--- A1/A1-StarterCode/interpreter.c
+++ A1/A1-StarterCode/interpreter.c
@@ -5,113 +5,141 @@
 #include "shellmemory.h"
 #include "shell.h"
 
-int MAX_ARGS_SIZE = 3;
+int MAX_ARGS_SIZE = 100;
 
 int help();
 int quit();
 int badcommand();
-int set(char* var, char* value);
+int set(char* var, char** value);
 int print(char* var);
 int run(char* script);
+int echo(char* var);
 int badcommandFileDoesNotExist();
 
 // Interpret commands and their arguments
 int interpreter(char* command_args[], int args_size){
-	int i;
+    int i;
 
-	if ( args_size < 1 || args_size > MAX_ARGS_SIZE){
-		return badcommand();
-	}
+    if ( args_size < 1 || args_size > MAX_ARGS_SIZE){
+        return badcommand();
+    }
 
 
-	for ( i=0; i<args_size; i++){ //strip spaces new line etc
-		command_args[i][strcspn(command_args[i], "\r\n")] = 0;
-	}
+    for ( i=0; i<args_size; i++){ //strip spaces new line etc
+        command_args[i][strcspn(command_args[i], "\r\n")] = 0;
+    }
 
-	if (strcmp(command_args[0], "help")==0){
-	    //help
-	    if (args_size != 1) return badcommand();
-	    return help();
-	
-	} else if (strcmp(command_args[0], "quit")==0) {
-		//quit
-		if (args_size != 1) return badcommand();
-		return quit();
+    if (strcmp(command_args[0], "help")==0){
+        //help
+        if (args_size != 1) return badcommand();
+        return help();
 
-	} else if (strcmp(command_args[0], "set")==0) {
-		//set
-		if (args_size != 3) return badcommand();	
-		return set(command_args[1], command_args[2]);
-	
-	} else if (strcmp(command_args[0], "print")==0) {
-		if (args_size != 2) return badcommand();
-		return print(command_args[1]);
-	
-	} else if (strcmp(command_args[0], "run")==0) {
-		if (args_size != 2) return badcommand();
-		return run(command_args[1]);
-	// my_ls
-	} else if (strcmp(command_args[0], "my_ls")==0) {
-		if (args_size != 1) return badcommand();
-		return system("ls -1");
-	
-	} else return badcommand();
+    } else if (strcmp(command_args[0], "quit")==0) {
+        //quit
+        if (args_size != 1) return badcommand();
+        return quit();
+
+    } else if (strcmp(command_args[0], "set")==0) {
+        //set
+        if (args_size > 7) {
+            printf("Bad command: Too many tokens\n");
+            return 1;
+        }
+        return set(command_args[1], command_args + 2);
+
+    } else if (strcmp(command_args[0], "print")==0) {
+        if (args_size != 2) return badcommand();
+        return print(command_args[1]);
+
+    } else if (strcmp(command_args[0], "echo") == 0) {
+        if (args_size != 2) return badcommand();
+        return echo(command_args[1]);
+    } else if (strcmp(command_args[0], "run")==0) {
+        if (args_size != 2) return badcommand();
+        return run(command_args[1]);
+    } else if (strcmp(command_args[0], "my_ls")==0) {
+        if (args_size != 1) return badcommand();
+        return system("ls -1");
+    } else return badcommand();
 }
 
 int help(){
 
-	char help_string[] = "COMMAND			DESCRIPTION\n \
+    char help_string[] = "COMMAND			DESCRIPTION\n \
 help			Displays all the commands\n \
 quit			Exits / terminates the shell with “Bye!”\n \
 set VAR STRING		Assigns a value to shell memory\n \
 print VAR		Displays the STRING assigned to VAR\n \
 run SCRIPT.TXT		Executes the file SCRIPT.TXT\n ";
-	printf("%s\n", help_string);
-	return 0;
+    printf("%s\n", help_string);
+    return 0;
 }
 
 int quit(){
-	printf("%s\n", "Bye!");
-	exit(0);
+    printf("%s\n", "Bye!");
+    exit(0);
 }
 
 int badcommand(){
-	printf("%s\n", "Unknown Command");
-	return 1;
+    printf("%s\n", "Unknown Command");
+    return 1;
 }
 
 // For run command only
 int badcommandFileDoesNotExist(){
-	printf("%s\n", "Bad command: File not found");
-	return 3;
+    printf("%s\n", "Bad command: File not found");
+    return 3;
 }
 
-int set(char* var, char* value){
-
-	char *link = "=";
-	char buffer[1000];
-	strcpy(buffer, var);
-	strcat(buffer, link);
-	strcat(buffer, value);
-
-	mem_set_value(var, value);
-
-	return 0;
-
+int set(char* var, char** value){
+    mem_set_value(var, value);
+    return 0;
 }
 
 int print(char* var){
-	printf("%s\n", mem_get_value(var)); 
-	return 0;
+    char **tokens = mem_get_value(var);
+    if (tokens == NULL) {
+        printf("Variable does not exist\n");
+        return 0;
+    } else {
+        int i;
+        for (i = 0; tokens[i]; ++i) {
+            if (i != 0) {
+                printf(" ");
+            }
+            printf("%s", tokens[i]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
 
-int run(char* script){
-	int errCode = 0;
-	char line[1000];
-	char *line_piece;
-	FILE *p = fopen(script,"rt");  // the program is in a file
+int echo(char* var) {
+    if (var[0] == '$') {
+        char **tokens = mem_get_value(var+1);
+        if (tokens != NULL) {
+            int i;
+            for (i = 0; tokens[i]; ++i) {
+                if (i != 0) {
+                    printf(" ");
+                }
+                printf("%s", tokens[i]);
+            }
+        }
+    } else {
+        printf("%s", var);
+    }
+    printf("\n");
+}
 
-	if(p == NULL){
+
+int run(char* script){
+    int errCode = 0;
+    char line[1000];
+    char *line_piece;
+    FILE *p = fopen(script,"rt");  // the program is in a file
+
+    if(p == NULL){
 		return badcommandFileDoesNotExist();
 	}
 
